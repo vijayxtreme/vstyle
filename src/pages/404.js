38c73494afb73ef1800bfaad11e2a25a68175918
Vijay Menon <vijaymenonx@gmail.com>
2021-03-12 12:44:37 -0800

    updates

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,8 +8,8 @@ import fail from "../images/fail.jpg"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <Container className="mt-5 pt-5">
-    <p><img src={fail} /></p>
+    <Container className="text-center mt-5 pt-5">
+    <p><img src={fail} style={{width:`404px`, height: `auto`}} /></p>
     <p>You just hit a route that doesn't exist yet... the sadness.</p>
     </Container>
   </Layout>

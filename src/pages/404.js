fb1dfc2710c35b42bb7cc1d44c20ec3f6b5dc34b
Vijay Menon <vijaymenonx@gmail.com>
2021-03-12 12:45:46 -0800

    updates

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,13 +4,15 @@ import Layout from "../components/layout"
 import {Container} from 'react-bootstrap'
 import SEO from "../components/seo"
 import fail from "../images/fail.jpg"
+import Breadcrumb from "../components/breadcrumb"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
+    <Breadcrumb title="404" />
     <Container className="text-center mt-5 pt-5">
     <p><img src={fail} style={{width:`404px`, height: `auto`}} /></p>
-    <p>You just hit a route that doesn't exist yet... the sadness.</p>
+    <p>You just hit a route that doesn't exist yet... the sadness. Go <a href="/">Home</a>?</p>
     </Container>
   </Layout>
 )

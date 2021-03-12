9e7d37f1a7320b33ecd3b6c239ca1eacaee04bf0
Vijay Menon <vijaymenonx@gmail.com>
2021-03-12 12:42:06 -0800

    updates

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import {Container} from 'react-bootstrap'
 import SEO from "../components/seo"
+import fail from "../images/fail.jpg"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <Container>
+    <Container className="mt-5 pt-5">
+    <p><img src={fail} /></p>
     <p>You just hit a route that doesn't exist yet... the sadness.</p>
     </Container>
   </Layout>
